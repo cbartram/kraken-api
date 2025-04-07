e9f480d7bfdb6af8d11ef4ef95643b692aa22f3f
cbartram <cbartram@gatech.edu>
2025-04-07 16:58:02 -0400

    feat: create dedicated component for kraken sockets

--- main.go
+++ main.go
@@ -67,8 +67,6 @@ func main() {
 	//	logrus.Fatalf("failed to import plugin packs: %v", err)
 	//}
 
-	go src.RegisterNewSocketServer()
-
 	// Registers a new go routine listening to the stripe-webhooks channel. New messages are enqueued when the /api/v1/stripe/webhook
 	// endpoint is called and this function consumes the messages with a 3-second delay in between each message resolving eventual consistency
 	// issues with both cognito and stripe when many events are sent at checkout.
